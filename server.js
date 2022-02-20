ba5be9ed7b6c862ac0299348cd7547f79585388a
James McArthur <77163783+jamezmca@users.noreply.github.com>
2022-02-20 10:52:40 -0800

    refactored code

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 //can restart nodemon by typing rs in terminal
-require('dotenv').config()
-const cheerio = require('cheerio')
-const axios = require('axios')
 const { google } = require('googleapis')
 const { readDb, writeDb } = require('./dbFunctions') //has to be an object
 const OAuth2Data = require("./client_secret.json");
@@ -11,7 +8,6 @@ const port = 5000
 app.use(require('cors')())
 
 const videoId = '9gqVvjfCC1o'
-const baseAPIUrl = 'https://www.googleapis.com/youtube/v3'
 const CLIENT_ID = OAuth2Data.web.client_id
 const CLIENT_SECRET = OAuth2Data.web.client_secret
 const REDIRECT_URL = OAuth2Data.web.redirect_uris[0]
@@ -49,10 +45,7 @@ function cron(ms, fn) {
 }
 
 // setup cron job
-cron(900000, async () => {
-    const res = await axios.get('http://localhost:5000/test')
-    const { data } = res
-    console.log(data)
+cron(60000, async () => {
     const tokens = readDb('tokens.json')
     oauth2Client.setCredentials(tokens)
 
@@ -95,67 +88,6 @@ app.get('/', async (req, res) => { //gets redirected here
     const { refresh_token, access_token } = tokens
     writeDb({ refresh_token, access_token }, 'tokens.json')
     res.status(200).send('Success')
-})
-
-
-app.get('/test', (req, res) => {
-    res.send('Nice')
-})
-
-
-// app.get('/', async (req, res) => { //gets redirected here
-//     //call youtube api get numbers of views on video
-//     const { code } = req.query
-//     try {
-//         const { data: html } = await axios.get('https://www.youtube.com/watch?v=MBqS1kYzwTc')
-//         const $ = cheerio.load(html)
-//         res.status(200).send({ html: $.html() })
-
-//     } catch (err) {
-//         res.status(500).send({ message: 'Server error :\'(' })
-//     }
-// })
-
-// app.get('/:url', async (req, res) => {
-//     console.log('nice')
-//     //going to demonstrate how to use queries or params in the case when you want to look at stats of someone elses video
-//     const { url } = req.params
-//     const { url2 } = req.query
-//     try {
-//         const { data: html } = await axios.get('https://www.youtube.com/watch?v=MBqS1kYzwTc')
-//         const $ = cheerio.load(html)
-//         // do res.status(200).send(html) to render the page to show what ur getting
-//         // res.json(data)
-//         res.status(200).send({ html: $.html() })
-
-//     } catch (err) {
-//         res.status(500).send(`<div>ERROR (propbably a shitty url): <h1>${url}!</h1><h1>${url2}</h1></div>`) //replace later
-//     }
-
-// })
-// app.get('/link/:url', async (req, res) => {
-//     console.log('nice')
-//     //going to demonstrate how to use queries or params in the case when you want to look at stats of someone elses video
-//     const { url } = req.params
-//     const { url2 } = req.query
-//     try {
-//         const { data: html } = await axios.get('https://www.youtube.com/watch?v=MBqS1kYzwTc')
-//         const $ = cheerio.load(html)
-//         // do res.status(200).send(html) to render the page to show what ur getting
-//         // res.json(data)
-//         res.status(200).send({ html: $.html() })
-
-//     } catch (err) {
-//         res.status(500).send(`<div>ERROR (propbably a shitty url): <h1>${url}!</h1><h1>${url2}</h1></div>`) //replace later
-//     }
-
-// })
-
-
-
-app.post('/', (req, res) => {
-    //going to demonstrate how to use body information and headers
-    const { data } = req.body
 })
 
 
